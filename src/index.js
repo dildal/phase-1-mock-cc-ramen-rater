76c9f67cdaf770b5be3c16c8a5f9648f1b52c1ea
Michael Dally <mddally@gmail.com>
2022-02-09 10:47:25 -0500

    Display ramen images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+const URL = 'http://localhost:3000/ramens'
+
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => displayRamen(data))
+})
+
+function displayRamen(allRamen){
+    const ramenMenu = document.querySelector('#ramen-menu');
+    allRamen.forEach(ramen => {
+        const ramenImg = document.createElement('img');
+        ramenImg.src = ramen.image;
+        ramenMenu.appendChild(ramenImg)
+    })
+}
